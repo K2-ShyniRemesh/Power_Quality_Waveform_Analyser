f2d25740f0a50b59bd37656a5af361b57d89fac3
K2-ShyniRemesh <kashyap2.shyniremesh@live.uwe.ac.uk>
2026-04-18 14:31:09 +0100

    adding a function (checkFormat) that checks the format of the first few lines and prints them using fscanf and fget(to fix having to look through the headings).

--- waveform.h
+++ waveform.h
@@ -39,3 +39,25 @@ typedef struct {
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠔⠉⠀⠀⢽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣼⣻⢷⣯⡽⣞⣷⣻⡼⣡⢋⡔⠣⠜⡐⢐⠠⡓⣤⣙⣲⣽⣻⢷
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡿⣽⣞⣷⣻⡴⣣⢜⡱⣊⡕⣊⠠⡙⡰⣭⢷⣯⣿⢿⠀⠀⠀⠀⠀⠀  ⠀
 */
+
+/*
+//Creates a new txt file and writes the below outputs onto it
+FILE *Output = fopen("../output.txt","w");
+
+fprintf(Output,"Phase A RMS:");
+fprintf(Output,"\nPhase A Peak-to-Peak:");
+fprintf(Output,"\nPhase A DC offset:");
+fprintf(Output,"\nPhase B RMS");
+fprintf(Output,"\nPhase B Peak-to-Peak:");
+fprintf(Output,"\nPhase B DC offset:");
+fprintf(Output,"\nPhase C RMS");
+fprintf(Output,"\nPhase B Peak-to-Peak:");
+fprintf(Output,"\nPhase B DC offset:");
+fprintf(Output,"\nClipped samples:");
+fprintf(Output,"\nFrequency range:");
+fprintf(Output,"\nPower factor range:");
+fprintf(Output,"\nTHD range:");
+fprintf(Output,"\nStandard deviation:");
+
+printf("Output saved as output.txt in the main program directory");
+fclose(Output);*/
